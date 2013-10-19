11ab4096a59613957a8bca5aa3d32be067dec959
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-10-19 19:32:25 -0300

    Add arithmetic comparison tests

--- src/test/numbertest.cpp
+++ src/test/numbertest.cpp
@@ -1,5 +1,11 @@
 #include "tests_common.hpp"
 
+#define TEST_NUMBER_COMPARISON(num1, num2, symb, eq)\
+TEST_CONDITION(\
+    rbb::number(num1).send_msg(rbb::symbol(symb)).send_msg(rbb::number(num2)) == rbb::boolean(eq),\
+    printf("%lf %s %lf doesn't equal %s!\n",\
+           (double) num1, symb, (double) num2, eq? "true" : "false"))
+
 TESTS_INIT()
     TEST_CONDITION(
         rbb::number(15).__value.type & rbb::value_t::number_t,
@@ -13,4 +19,22 @@ TESTS_INIT()
     TEST_CONDITION(
         rbb::number(15).__value.integer == 15,
         puts("rbb::number doesn't initialize integer numbers correctly"))
+    
+    TEST_NUMBER_COMPARISON(15, 15, "!=", false)
+    TEST_NUMBER_COMPARISON(11, 13, "!=", true)
+    TEST_NUMBER_COMPARISON(12.1, 12.1, "!=", false)
+    TEST_NUMBER_COMPARISON(11, 13.1, "!=", true)
+    TEST_NUMBER_COMPARISON(12.22, 12.222, "!=", true)
+    TEST_NUMBER_COMPARISON(14.0000001, 14, "!=", true)
+    TEST_NUMBER_COMPARISON(13, 12, "==", false)
+    TEST_NUMBER_COMPARISON(14.2, 14.2, "!=", false)
+    
+    TEST_NUMBER_COMPARISON(11.000001, 11, ">", true)
+    TEST_NUMBER_COMPARISON(11, 12, "<", true)
+    TEST_NUMBER_COMPARISON(15.3, 13, "<", false)
+    TEST_NUMBER_COMPARISON(200, 200.01, ">", false)
+    TEST_NUMBER_COMPARISON(25, 25, ">=", true)
+    TEST_NUMBER_COMPARISON(30, 30, "<=", true)
+    TEST_NUMBER_COMPARISON(12, 13, ">=", false)
+    TEST_NUMBER_COMPARISON(14, 12, "<=", false)
 TESTS_END()
