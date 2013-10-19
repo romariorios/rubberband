cf2b27e462ac6abb4a726f88d1dec69395aa288a
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-10-18 21:10:21 -0300

    Add boolean object tests

---
+++ src/test/booleantest.cpp
@@ -0,0 +1,19 @@
+#include "tests_common.hpp"
+
+int main()
+{
+    TESTS_INIT()
+    
+    rbb::object true_obj = rbb::boolean(true);
+    rbb::object false_obj = rbb::boolean(false);
+    rbb::object equals = rbb::symbol("==");
+    
+    TEST_CONDITION(true_obj.__value.boolean == true, puts("True object is false"))
+    TEST_CONDITION(false_obj.__value.boolean == false, puts("False object is true"))
+    TEST_CONDITION(true_obj.send_msg(equals).send_msg(true_obj).__value.type == rbb::value_t::boolean_t,
+                   puts("The comparison block isn't returning a boolean"))
+    TEST_CONDITION(true_obj.send_msg(equals).send_msg(true_obj) == true_obj, puts("(true == true) != true"))
+    TEST_CONDITION(true_obj.send_msg(true_obj) == rbb::empty(), puts("Boolean responds to a boolean message (it shouldn't)"))
+    
+    TESTS_END()
+}
