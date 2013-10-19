7fd8b2d271b03fb1a1561b3bcaa6489888213bff
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-10-19 12:14:58 -0300

    Add tests for the empty object

---
+++ src/test/emptytest.cpp
@@ -0,0 +1,20 @@
+#include "tests_common.hpp"
+
+TESTS_INIT()
+    rbb::object empty_equals_empty = rbb::empty().send_msg(rbb::symbol("==")).send_msg(rbb::empty());
+
+    TEST_CONDITION(
+        empty_equals_empty.__value.type == rbb::value_t::boolean_t,
+        printf("\"Empty equals empty\" isn't a boolean (Actual value: %d)\n", empty_equals_empty.__value.type))
+    TEST_CONDITION(
+        empty_equals_empty == rbb::boolean(true), puts("Empty doesn't equal empty"))
+    
+    rbb::object empty_differs_empty = rbb::empty().send_msg(rbb::symbol("!=")).send_msg(rbb::empty());
+    
+    TEST_CONDITION(
+        empty_differs_empty == rbb::boolean(false),
+        puts("\"Empty differs empty\" equals true"))
+    TEST_CONDITION(
+        rbb::empty().send_msg(rbb::symbol("==")).send_msg(rbb::symbol("duvnis")) == rbb::boolean(false),
+        puts("Empty equals a random symbol"))
+TESTS_END()
