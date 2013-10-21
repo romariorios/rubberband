108479457090c6ef7471a23d4550b265d40ad5e7
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-10-21 19:12:23 -0300

    Add length test

--- src/test/listtest.cpp
+++ src/test/listtest.cpp
@@ -10,4 +10,7 @@ TESTS_INIT()
     TEST_CONDITION(
         l.send_msg(rbb::symbol("!=")).send_msg(l) == rbb::boolean(false),
         puts("A list differs from itself"))
+    TEST_CONDITION(
+        l.send_msg(rbb::symbol("?|")) == rbb::number(3),
+        puts("The list doesn't correctly report its size"))
 TESTS_END()
