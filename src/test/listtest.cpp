9f6283da69265653dddcc691620d320ec460542b
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-10-21 23:56:33 -0300

    Add concatenation test

--- src/test/listtest.cpp
+++ src/test/listtest.cpp
@@ -39,4 +39,26 @@ TESTS_INIT()
     TEST_CONDITION(
         l.send_msg(rbb::number(0)) == rbb::symbol("number_list"),
         puts("Assigning an object to a position has no effect"))
+    
+    rbb::object other_numbers[] = {rbb::number(2), rbb::number(3), rbb::number(5)};
+    rbb::object l2 = rbb::list(other_numbers, 3);
+    rbb::object l3 = l.send_msg(rbb::symbol("+")).send_msg(l2);
+    
+    rbb::object all_numbers[] = {
+        rbb::symbol("number_list"), rbb::number(4), rbb::number(9),
+        rbb::number(2), rbb::number(3), rbb::number(5)
+    };
+    rbb::object l4 = rbb::list(all_numbers, 6);
+    
+    bool l3_eq_l4 = true;
+    
+    for (int i = 0; i < 6; ++i) {
+        if (l3.send_msg(rbb::number(i)) == l4.send_msg(rbb::number(i))) {
+        } else {
+            l3_eq_l4 = false;
+            break;
+        }
+    }
+    
+    TEST_CONDITION(l3_eq_l4, puts("Concatenation doesn't work"));
 TESTS_END()
