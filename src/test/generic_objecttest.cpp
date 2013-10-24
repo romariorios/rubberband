b40a33b1030a31834cef7461d6f4d1eee8b9a1c4
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-10-24 20:08:33 -0300

    Add field request tests

--- src/test/generic_objecttest.cpp
+++ src/test/generic_objecttest.cpp
@@ -12,4 +12,19 @@ TESTS_INIT()
     TEST_CONDITION(
         g_object.send_msg(rbb::symbol("!=")).send_msg(g_object) == rbb::boolean(false),
         puts("g_object != g_object doesn't equal false"))
+    TEST_CONDITION(
+        g_object.send_msg(rbb::symbol("a")) == rbb::number(100),
+        puts("Can't find field a"))
+    TEST_CONDITION(
+        g_object.send_msg(rbb::symbol("b")) == rbb::number(200),
+        puts("Can't find field b"))
+    TEST_CONDITION(
+        g_object.send_msg(rbb::symbol("c")) == rbb::number(300),
+        puts("Can't find field c"))
+    TEST_CONDITION(
+        g_object.send_msg(rbb::symbol("d")) == rbb::empty(),
+        puts("Generic object should answer empty for a non-existing field"))
+    TEST_CONDITION(
+        g_object.send_msg(rbb::number(100)) == rbb::empty(),
+        puts("Generic object should answer empty for invalid field names (number 100)"))
 TESTS_END()
