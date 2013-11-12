928614bf4bfc7f64e57a5fa91d8ac28c4719b4f5
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-11-12 14:02:31 -0300

    Add shell script for building and running tests

---
+++ src/test/runtests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z $CXX ]
+then
+    echo "No CXX defined"
+    exit -1
+fi
+
+for t in *test.cpp
+do
+    test_name=${t%\.*}
+    echo "--- $test_name ---"
+    $CXX $t ../core/object.cpp ../core/symbol.cpp ../core/block.cpp -o $test_name $CXX_FLAGS &&
+    ./$test_name &&
+    rm $test_name
+done
+
