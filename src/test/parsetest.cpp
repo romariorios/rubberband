bb2f5acbc638f4f86af47d75315fc21983332f68
Luiz Rom?rio Santana Rios <luizromario@gmail.com>
2014-08-25 19:02:28 -0300

    Fix some parsetest errors

--- src/test/parsetest.cpp
+++ src/test/parsetest.cpp
@@ -14,5 +14,7 @@ TESTS_INIT()
     
     TEST_CONDITION(
         result_number == rbb::number(10),
-        printf("Program { !10 } returns %s\n", result_number))
+        printf(
+            "Program { !10 } returns %s\n",
+            rbb::object_to_string(result_number).c_str()))
 TESTS_END()
