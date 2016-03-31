d1585b5ed75f5191d4694109013756496b3f582d
Luiz Romário Santana Rios <luizromario@gmail.com>
2016-03-30 21:27:18 -0300

    Add one more literal definition test

--- src/test/literal_definitiontest.cpp
+++ src/test/literal_definitiontest.cpp
@@ -18,5 +18,13 @@ TESTS_INIT()
                 "Error trying to parse user-defined literal ' (expected value: %s, got: %s)\n",
                 number(12).to_string().c_str(),
                 literal_value.to_string().c_str()))
+
+        auto table_with_literals = dummy_master.parse(":a -> ', b -> ' + 10");
+        TEST_CONDITION(
+            table_with_literals << symbol("a") == number(12) &&
+            table_with_literals << symbol("b") == number(22),
+            printf(
+                "Error: unexpected parsed value %s",
+                table_with_literals.to_string().c_str()))
     }
 TESTS_END()
