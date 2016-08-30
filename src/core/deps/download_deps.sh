fa7061a7fd293584d3e3610eb60485dfd0cd48dc
Luiz Romário Santana Rios <luizromario@gmail.com>
2016-08-30 10:02:14 -0300

    Create script to download dependencies automatically

---
+++ src/core/deps/download_deps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl https://www.sqlite.org/src/raw/tool/lemon.c\?name\=09a96bed19955697a5e20c49ad863ec2005815a2 > lemon.c &&
+curl https://www.sqlite.org/src/raw/tool/lempar.c\?name\=147e42a5cd83ce38275fde0d07a5df3330cb9b3b > lempar.c
