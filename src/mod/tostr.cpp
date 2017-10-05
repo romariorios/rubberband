4ff9b3d3aa9e4690a48095404bc79d89a81ae628
Luiz Romário Santana Rios <luizromario@gmail.com>
2017-10-05 18:58:51 -0300

    Add ability to print more objects with the io mod

--- src/mod/tostr.cpp
+++ src/mod/tostr.cpp
@@ -1,3 +1,24 @@
+// Rubberband tostr module: turn objects into strings
+// Copyright (c) 2017  Luiz Romário Santana Rios <luizromario at gmail dot com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 #include <error.hpp>
 #include <interfaces.hpp>
 #include <object.hpp>
@@ -50,6 +71,11 @@ public:
     object mk_string;
 };
 
+static string data_tostr(value_t::data_ptr &&data)
+{
+    return data->to_string();
+}
+
 static string obj_tostr(const object &obj)
 {
     switch (obj.__value.type) {
@@ -63,6 +89,8 @@ static string obj_tostr(const object &obj)
         return *obj.__value.symbol();
     case value_t::boolean_t:
         return obj.__value.boolean()? "?t" : "?f";
+    case value_t::data_t:
+        return data_tostr(move(obj.__value.data()));
     default:
         break;
     }
